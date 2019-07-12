8c915f54e40f8429ad2d8df3950d446b971b4de6
Howard <howard.lin314@gmail.com>
2019-07-12 11:21:09 -0700

    dev update

--- posts/2019-07-11-about_hakyll.md
+++ posts/2019-07-11-about_hakyll.md
@@ -14,18 +14,28 @@ After you create and build the project you have the following:
 Here's how hakyll generates each page of the website (I think, I'm still figuring it out)  
 
 The index:  
+
 1. Apply the proper templates to make index.html have the list of posts  
 2. Take that index.html and make it $content$  
 3. Take the default.html and replace the instances of $content$ on the template with index.html  
 4. Export that as index.html  
 
 The posts and stuff:  
+
 1. Take the post's markdown data  
 2. Pull out the metadata, called the 'context'  
 3. Apply whatever templates you want to it  
 
-Here's how I made the favorite section on the index:   
+Here's how I made the favorite section on the index: 
+  
 1. Add a favorite tag to the metadata of the post I want to favorite  
 2. Make a new template called favorite-list  
 3. Use the hakyll DSL syntax to make the template do the same thing as post-list but only if the favorite tag is present  
 4. Add to index.html a partial that uses favorite-list  
+
+Using pandocCompilerWith to make stuff like laTeX
+
+\\[ \\ln x = \\int_{-\\infty}^x \\frac 1 y \\, dy . \\]
+
+I followed this: http://travis.athougies.net/posts/2013-08-13-using-math-on-your-hakyll-blog.html
+The idea is that you just need to set the proper extensions and options for pandoc's writing
--- site.hs
+++ site.hs
@@ -4,71 +4,89 @@ import           Data.Monoid (mappend)
 import           Hakyll
 import qualified Data.Set as S
 import 			 Text.Pandoc.Options
-
+import			 Skylighting
 --------------------------------------------------------------------------------
-pandocCustomCompiler = 
-	let defaultExtensions = writerExtensionsdefaultHakyllWriterOptions
-		newExtensions = S.union githubMarkdownExtensions defaultExtensions
-		writerOptions = defaultHakyllOptions{
-							writerExtensions = newExtensions,
-							writerHTMLMathMethod = MathJax ""
-						}
-	in pandocCompilerWith defaultHakyllReaderOptions writerOptions
+syntaxHighlightingStyle :: Style
+syntaxHighlightingStyle = haddock
+
+generateSyntaxStyle :: IO ()
+generateSyntaxStyle = do
+	let css = styleToCss syntaxHighlightingStyle
+	writeFile "css/syntax.css" css
+	return ()
+
+
+pandocCustomCompiler =
+    let mathExtensions = [Ext_tex_math_dollars, Ext_tex_math_double_backslash,
+                          Ext_latex_macros]
+        codeExtensions = [Ext_fenced_code_blocks, Ext_backtick_code_blocks, Ext_fenced_code_attributes]
+
+        defaultExtensions = writerExtensions defaultHakyllWriterOptions
+        newExtensions = foldr S.insert defaultExtensions (mathExtensions <> codeExtensions <> githubMarkdownExtensions)
+        writerOptions = defaultHakyllWriterOptions {
+                          writerExtensions = newExtensions,
+                          writerHTMLMathMethod = MathJax "",
+                          writerHighlightStyle = Just syntaxHighlightingStyle
+                        }
+    in pandocCompilerWith defaultHakyllReaderOptions writerOptions
 
 main :: IO ()
-main = hakyll $ do
+main = do
 
-    match "images/*" $ do
-        route   idRoute
-        compile copyFileCompiler
+	generateSyntaxStyle
 
-    match "css/*" $ do
-        route   idRoute
-        compile compressCssCompiler
-        
-    match (fromList ["about.rst", "contact.markdown"]) $ do
-        route   $ setExtension "html"
-        compile $ pandocCustomCompiler
-            >>= loadAndApplyTemplate "templates/default.html" defaultContext
-            >>= relativizeUrls
+	hakyll $ do
+	    match "images/*" $ do
+	        route   idRoute
+	        compile copyFileCompiler
 
-    match "posts/*" $ do
-        route $ setExtension "html"
-        compile $ pandocCustomCompiler
-            >>= loadAndApplyTemplate "templates/post.html"    postCtx
-            >>= loadAndApplyTemplate "templates/default.html" postCtx
-            >>= relativizeUrls
+	    match "css/*" $ do
+	        route   idRoute
+	        compile compressCssCompiler
+	        
+	    match (fromList ["about.rst", "contact.markdown"]) $ do
+	        route   $ setExtension "html"
+	        compile $ pandocCustomCompiler
+	            >>= loadAndApplyTemplate "templates/default.html" defaultContext
+	            >>= relativizeUrls
 
-    create ["archive.html"] $ do
-        route idRoute
-        compile $ do
-            posts <- recentFirst =<< loadAll "posts/*"
-            let archiveCtx =
-                    listField "posts" postCtx (return posts) `mappend`
-                    constField "title" "Archives"            `mappend`
-                    defaultContext
+	    match "posts/*" $ do
+	        route $ setExtension "html"
+	        compile $ pandocCustomCompiler
+	            >>= loadAndApplyTemplate "templates/post.html"    postCtx
+	            >>= loadAndApplyTemplate "templates/default.html" postCtx
+	            >>= relativizeUrls
 
-            makeItem ""
-                >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
-                >>= loadAndApplyTemplate "templates/default.html" archiveCtx
-                >>= relativizeUrls
+	    create ["archive.html"] $ do
+	        route idRoute
+	        compile $ do
+	            posts <- recentFirst =<< loadAll "posts/*"
+	            let archiveCtx =
+	                    listField "posts" postCtx (return posts) `mappend`
+	                    constField "title" "Archives"            `mappend`
+	                    defaultContext
+
+	            makeItem ""
+	                >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
+	                >>= loadAndApplyTemplate "templates/default.html" archiveCtx
+	                >>= relativizeUrls
 
 
-    match "index.html" $ do
-        route idRoute
-        compile $ do
-            posts <- recentFirst =<< loadAll "posts/*"
-            let indexCtx =
-                    listField "posts" postCtx (return posts) `mappend`
-                    constField "title" "Home"                `mappend`
-                    defaultContext
+	    match "index.html" $ do
+	        route idRoute
+	        compile $ do
+	            posts <- recentFirst =<< loadAll "posts/*"
+	            let indexCtx =
+	                    listField "posts" postCtx (return posts) `mappend`
+	                    constField "title" "Home"                `mappend`
+	                    defaultContext
 
-            getResourceBody
-                >>= applyAsTemplate indexCtx
-                >>= loadAndApplyTemplate "templates/default.html" indexCtx
-                >>= relativizeUrls
+	            getResourceBody
+	                >>= applyAsTemplate indexCtx
+	                >>= loadAndApplyTemplate "templates/default.html" indexCtx
+	                >>= relativizeUrls
 
-    match "templates/*" $ compile templateCompiler
+	    match "templates/*" $ compile templateCompiler
 
 --------------------------------------------------------------------------------
 postCtx :: Context String
