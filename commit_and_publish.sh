e3fa00dc698ed2c0d789efc5b67057d359ae3761
Lily Lin <rctcwyvrn@gmail.com>
2020-08-19 20:51:37 -0700

    Create docs directory for publishing

--- commit_and_publish.sh
+++ commit_and_publish.sh
@@ -13,6 +13,9 @@ echo "-----------------Building new files"
 cabal new-run site clean
 cabal new-run site build
 
+# Copy files to publishing directory
+cp -R _site/* docs
+
 # commit to develop branch, probably uneccessary?
 echo "-----------------Commiting to develop branch"
 git add -A
