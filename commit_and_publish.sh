8e8fb30a878eaeb32112be613374bef91c1cb184
Lily Lin <rctcwyvrn@gmail.com>
2020-08-19 20:49:49 -0700

    Test out new github pages functionality
    See if I can just publish from this branch now

--- commit_and_publish.sh
+++ commit_and_publish.sh
@@ -8,9 +8,6 @@ git checkout develop
 #Run tag fixer
 python tag-fixer.py
 
-#Recompile site.hs
-stack build
-
 # Build new files
 echo "-----------------Building new files"
 cabal new-run site clean
@@ -28,34 +25,34 @@ fi
 
 git push origin develop
 
-# Get previous files
-echo "-----------------Fetching and checking out to master"
-git fetch --all
-git checkout -b master --track origin/master
+# # Get previous files
+# echo "-----------------Fetching and checking out to master"
+# git fetch --all
+# git checkout -b master --track origin/master
 
-# Overwrite existing files with new files
-echo "-----------------Writing new files"
-yes | cp -a -f _site/. .
+# # Overwrite existing files with new files
+# echo "-----------------Writing new files"
+# yes | cp -a -f _site/. .
 
-git checkout master
+# git checkout master
 
-# Commit
-echo "-----------------Committing to master"
-git add -A
+# # Commit
+# echo "-----------------Committing to master"
+# git add -A
 
-if [ "$1" != "" ]; then
-	git checkout master
-	git commit -m "$1"
-else
-	git checkout master
-	git commit -m "publishing"
-fi
+# if [ "$1" != "" ]; then
+# 	git checkout master
+# 	git commit -m "$1"
+# else
+# 	git checkout master
+# 	git commit -m "publishing"
+# fi
 
-# Push
-echo "-----------------Push"
-git push origin master #master:master
+# # Push
+# echo "-----------------Push"
+# git push origin master #master:master
 
-# Restoration
-echo "-----------------Restoring to original state"
-git checkout develop
-git branch -D master
+# # Restoration
+# echo "-----------------Restoring to original state"
+# git checkout develop
+# git branch -D master
